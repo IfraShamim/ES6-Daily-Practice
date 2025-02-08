08cd846fa4d10590a6bc6b65084e6e8834f998ec
Ifra Shamim <ifrashamim29@gmail.com>
2025-02-08 20:41:59 +0500

    Done!

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 
 const greet = (name) => `Hello ${name}`;
 console.log(greet("Ifra"));
+
+// Problem 2
+const multiply = (num1,num2 = 2) => {
+    return num1 * num2;
+}
+console.log(multiply(2,3));
+console.log(multiply(2));
+
